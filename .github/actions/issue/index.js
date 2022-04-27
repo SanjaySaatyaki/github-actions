992e0244d4ae3a196bc3e090577be836966c64d2
Sanjay Saatyaki <sanjayabs97@gmail.com>
2022-04-27 21:56:05 +0530

    change in constructor

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -8,7 +8,9 @@ async function run() {
     const body = core.getInput("body");
     const assignees = core.getInput("assignees");
 
-    const octokit = new github.GitHub(token);
+    const octokit = new Octokit({
+        auth: token,
+      });
 
     const response = await octokit.issues.create({
       // owner: github.context.repo.owner,
